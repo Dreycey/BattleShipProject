062e8eb6a027c244f548acb8086a71992b8bb830
Dreycey <albindreycey@gmail.com>
2021-02-13 14:14:14 -0700

    adding an example class and accompanying methods

---
+++ src/main/java/Example.java
@@ -0,0 +1,28 @@
+package io.collective;
+import java.util.HashMap;
+
+
+/*
+The Person class is in control of the Person Objects that are in  use during 
+game play.
+
+*/
+public class Person {
+    
+    public null returnsNull() {
+        return null;
+    }
+    
+    public int returnsInput(int input) {
+        return input;
+    }
+
+    public returnTrue() {
+        return true;
+    }
+
+    public returnFalse() {
+        return false;
+    }
+
+}
