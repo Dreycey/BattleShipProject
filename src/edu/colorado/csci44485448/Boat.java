075aa214667f10aeed656321e47592f41f89f5aa
cmlohrmann <c.m.lohrmann25@gmail.com>
2021-02-14 09:35:14 -0700

    created getStatus, tests for setStatus

--- src/edu/colorado/csci44485448/Boat.java
+++ src/edu/colorado/csci44485448/Boat.java
@@ -1,4 +1,10 @@
 package edu.colorado.csci44485448;
 
 public class Boat {
+
+    private String status = "Afloat";
+
+    public String getStatus(){
+        return status;
+    }
 }
--- src/edu/colorado/csci44485448/BoatTest.java
+++ src/edu/colorado/csci44485448/BoatTest.java
@@ -1,18 +1,24 @@
 package edu.colorado.csci44485448;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 class BoatTest {
-    @Test
-    public void testCreation() throws Exception{
-        Boat b = new Boat();
-    }
+
+    Boat boat = new Boat();
 
     @Test
     public void testGetStatus() throws Exception{
-        Boat b = new Boat();
         String defaultStatus = "Afloat";
-        Assertions.assertEquals(defaultStatus,b.getStatus());
+        Assertions.assertEquals(defaultStatus,boat.getStatus());
+    }
+
+    @Test
+    public void testSetStatus() throws Exception{
+        String newStatus = "Hit";
+
+        boat.setStatus(newStatus);
     }
 }
