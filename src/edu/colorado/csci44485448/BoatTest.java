a8b175660e009d42fb9df9e627725648c04311e3
cmlohrmann <c.m.lohrmann25@gmail.com>
2021-02-14 09:28:40 -0700

    getStatus test created

--- src/edu/colorado/csci44485448/BoatTest.java
+++ src/edu/colorado/csci44485448/BoatTest.java
@@ -1,12 +1,18 @@
 package edu.colorado.csci44485448;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
 class BoatTest {
     @Test
     public void testCreation() throws Exception{
         Boat b = new Boat();
+    }
+
+    @Test
+    public void testGetStatus() throws Exception{
+        Boat b = new Boat();
+        String defaultStatus = "Afloat";
+        Assertions.assertEquals(defaultStatus,b.getStatus());
     }
 }
