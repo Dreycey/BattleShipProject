01cb9025b5e400c9a993c4ca5f6860066398e7fa
cmlohrmann <c.m.lohrmann25@gmail.com>
2021-02-14 08:58:41 -0700

    Set up testing

---
+++ src/edu/colorado/csci44485448/Boat.java
@@ -0,0 +1,4 @@
+package edu.colorado.csci44485448;
+
+public class Boat {
+}
---
+++ src/edu/colorado/csci44485448/BoatTest.java
@@ -0,0 +1,12 @@
+package edu.colorado.csci44485448;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoatTest {
+    @Test
+    public void testCreation() throws Exception{
+        Boat b = new Boat();
+    }
+}
