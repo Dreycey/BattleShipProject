2d439c7ca8c5a2aa812ae239bf87822912e37433
cmlohrmann <c.m.lohrmann25@gmail.com>
2021-02-14 11:04:13 -0700

    getters finished, testing status realignment

--- src/edu/colorado/csci44485448/BattleShip.java
+++ src/edu/colorado/csci44485448/BattleShip.java
@@ -3,12 +3,13 @@ package edu.colorado.csci44485448;
 public class BattleShip extends Boat{
 
     private static String name = "BattleShip";
+    private static int size = 4;
 
     public String getName(){
         return name;
     }
 
     public int getSize(){
-        return -1;
+        return size;
     }
 }
--- src/edu/colorado/csci44485448/BattleShipTest.java
+++ src/edu/colorado/csci44485448/BattleShipTest.java
@@ -46,4 +46,26 @@ class BattleShipTest {
     public void testGetSize() throws Exception{
         Assertions.assertEquals(4, boat.getSize());
     }
+
+    @Test
+    public void testRealignStatus() throws  Exception{
+        String[] coords = {"A1","A2","A3","A4"};
+        String[] hitCoords = {"A1","A2","A4"};
+        String[] sunkCoords = {};
+        String defaultStatus = "Afloat";
+
+        boat.setCoordinates(coords);
+
+        Assertions.assertEquals(defaultStatus, boat.getStatus());
+
+        boat.removeCoordinate("A3");
+
+        Assertions.assertEquals("Hit", boat.getStatus());
+
+        boat.removeCoordinate("A1");
+        boat.removeCoordinate("A2");
+        boat.removeCoordinate("A4");
+
+        Assertions.assertEquals("Sunk", boat.getStatus());
+    }
 }
