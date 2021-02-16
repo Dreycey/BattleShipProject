171c09ddff9de326cb33e8d79d32840d9cf70f62
Dreycey <albindreycey@gmail.com>
2021-02-15 19:28:25 -0700

    update tests

---
+++ src/main/java/BoatPackage/Person.java
@@ -0,0 +1,14 @@
+package BoatPackage;
+
+
+public class Person {
+    public int age = 28;
+
+    public int returnAge() {
+        return this.age;
+    }
+
+    public int returnAge2() {
+        return 29;
+    }
+}
