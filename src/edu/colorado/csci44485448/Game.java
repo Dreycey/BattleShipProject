6cd0275053f01fa791f60ec7d2b668c466b92e0b
cmlohrmann <c.m.lohrmann25@gmail.com>
2021-02-18 15:27:49 -0700

    setTurn good, testing for bad input just in case

--- src/edu/colorado/csci44485448/Game.java
+++ src/edu/colorado/csci44485448/Game.java
@@ -15,7 +15,7 @@ public class Game {
     }
 
     public void setTurn(int t){
-
+        turn = t;
     }
 
     public void playTurn(){
--- src/edu/colorado/csci44485448/GameTest.java
+++ src/edu/colorado/csci44485448/GameTest.java
@@ -5,18 +5,29 @@ import org.junit.jupiter.api.Test;
 
 class GameTest {
 
+    Game g = new Game();
+
     @Test
-    public void testMakeGame(){
-        Game g = new Game();
+    public void testTurns(){
 
         Assertions.assertEquals(1,g.getTurn());
 
         g.playTurn();
 
         Assertions.assertEquals(2,g.getTurn());
+    }
 
+    @Test
+    public void testSetTurn(){
         //oops, set turn to 1 just bc
         g.setTurn(1);
+
+        Assertions.assertEquals(1,g.getTurn());
+    }
+
+    @Test
+    public void testSetTurnBad(){
+        g.setTurn(10);
 
         Assertions.assertEquals(1,g.getTurn());
     }
