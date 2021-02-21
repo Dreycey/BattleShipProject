36b09d89a6cb816f9dd771e4127fa9217d62475f
Dreycey <albindreycey@gmail.com>
2021-02-21 15:13:51 -0700

    implimenting methods for the Game class

--- src/main/java/BoatPackage/Game.java
+++ src/main/java/BoatPackage/Game.java
@@ -1,10 +1,13 @@
-
 package BoatPackage;
+import java.util.*;
+
 
 public class Game {
 
     int turn;
-    String[] boats;
+    String[] boats = new String[]{"BoatPackage.Battleship","BoatPackage.Destroyer", "BoatPackage.Minesweeper"};
+    private Player playerOne;
+    private Player playerTwo;
 
     /*
     Description
@@ -13,9 +16,15 @@ public class Game {
         TODO: get rid of coupling.
 
      */
-    public Game() {
+    public Game() throws IllegalAccessException, InstantiationException, ClassNotFoundException {
         turn = 1;
-        boats = new String[]{"Minesweeper", "Destroyer", "Battleship"};
+        //boats = new String[]{"BoatPackage.Battleship","BoatPackage.Destroyer", "BoatPackage.Minesweeper"};
+
+        // Instantiate Players
+        String[][] coordPlayerOne = {{"A2","A3","A4"},{"B2","B3","B4","B5"},{"B4","B5"}};
+        String[][] coordPlayerTwo = {{"A2","A3","A4"},{"B2","B3","B4","B5"},{"B4","B5"}};
+        playerOne = new Player(setUpCoords(coordPlayerOne));
+        playerTwo = new Player(setUpCoords(coordPlayerTwo));
     }
 
     public int getTurn() {
@@ -28,19 +37,95 @@ public class Game {
         }
     }
 
+    public boolean isValidCoordinate(String[] inCoords){
+        int[][] convCoordArray = new int[inCoords.length][2];
+
+        // check each in array is good.
+        int indCounter = 0;
+        for (String coord : inCoords) {
+            int letter = (int) coord.charAt(0);
+            int number = Integer.parseInt(coord.substring(1));
+
+            if (number > 0 && number < 11 && letter > 64 && letter < 75) {
+                // create convCoordArray
+                int letterNew = letter - 64;
+                convCoordArray[indCounter][0] = letterNew;
+                convCoordArray[indCounter][1] = number;
+                indCounter++;
+            } else {
+                return false;
+            }
+        }
+
+        // check all next to one another.
+        int countIter = 0;
+        int prevRow = 100;
+        int prevCol = 100;
+        boolean isVertical = true;
+        for (int[] coordIndex  : convCoordArray) {
+            int row = coordIndex[0];
+            int column = coordIndex[1];
+
+            if (countIter == 1) {
+                int rowDist = row - prevRow;
+                int colDist = column - prevCol;
+                if (rowDist == 1 && colDist == 0) {
+                    isVertical = false;
+                }
+            }
+
+            if (countIter > 1) {
+                // now start checking
+                int rowDist = row - prevRow;
+                int colDist = column - prevCol;
+                if ((rowDist == 1 && colDist == 0 && isVertical == false) ||
+                    (rowDist == 0 && colDist == 1 && isVertical == true)) {}
+                else {
+                    return false;
+                }
+            }
+
+            prevRow = row;
+            prevCol = column;
+            countIter++;
+        }
+        return true;
+    }
+
     /*
     DESCRIPTION
-        This sets up all of the players
+        This sets up boat object and returns a list of boat object
+        with the input coordinates.
 
      INPUT:
-           1. PLayerObj
-           2. [minesweeper(), ..., destroyer()]
-           3. [[A1, C3, C4], []]
+           1. coordinates: [{"C3", "C4"}, []]
+     OUTPUT:
+            List<Boat> of boat objects with coordinates
 
     */
-    public int setUpBoard() {
-        playerObj
-        return 0; // if works, returns 0
+
+    public List<Boat> setUpCoords(String[][] inCoordinates) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        List<Boat> playerBoatList = new ArrayList<Boat>();
+
+        // instantiate boats, add
+        int boatCounterInd = 0;
+        for (String playerBoat : boats) {
+            // Create object from string name
+            //TODO: Decouple so player does the next two
+            Class<?> boatCall = Class.forName(playerBoat);
+            Boat boatObj = (Boat) boatCall.newInstance();
+            // print String name, set coordinates
+            System.out.println(boatObj.getName());
+            if (isValidCoordinate(inCoordinates[boatCounterInd])) {
+                boatObj.setCoordinates(inCoordinates[boatCounterInd]);
+            }
+            else {break;}
+            // add to list
+            playerBoatList.add(boatObj);
+            //increment counter
+            boatCounterInd++;
+        }
+        return playerBoatList; // if works, returns 0
     }
 
     public void playTurn() {
