e207e009f56d3917ed14984696d439b9bc4634a7
cmlohrmann <c.m.lohrmann25@gmail.com>
2021-03-01 13:19:40 -0700

    set up default tests for armored

---
+++ src/test/java/armoredCaptainsCabinTest.java
@@ -0,0 +1,28 @@
+import BoatPackage.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class armoredCaptainsCabinTest {
+
+    @Test
+    public void testConstructor() throws Exception{
+        String location = "A3";
+        captainsCabinType type = new armoredCaptainsCabin(location);
+    }
+
+    @Test
+    public void testGettersAndSetters() throws Exception{
+        String location = "A3";
+        captainsCabinType type = new armoredCaptainsCabin(location);
+
+        Assertions.assertEquals(location, type.getLoc());
+
+        String newLoc = "A4";
+        type.setLoc(newLoc);
+
+        Assertions.assertEquals(newLoc, type.getLoc());
+    }
+
+}
