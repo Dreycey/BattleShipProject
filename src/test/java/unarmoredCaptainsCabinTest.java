deaddc036f2df439dcd9283a9ca8e50ea4cdd4ec
cmlohrmann <c.m.lohrmann25@gmail.com>
2021-03-01 13:12:36 -0700

    super class getters/setters tested

--- src/test/java/unarmoredCaptainsCabinTest.java
+++ src/test/java/unarmoredCaptainsCabinTest.java
@@ -1,5 +1,6 @@
 import BoatPackage.captainsCabinType;
 import BoatPackage.unarmoredCaptainsCabin;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +11,19 @@ class unarmoredCaptainsCabinTest {
     public void testConstructor() throws Exception{
         String location = "A3";
         captainsCabinType type = new unarmoredCaptainsCabin(location);
+    }
+
+    @Test
+    public void testGettersAndSetters() throws Exception{
+        String location = "A3";
+        captainsCabinType type = new unarmoredCaptainsCabin(location);
+
+        Assertions.assertEquals(location, type.getLoc());
+
+        String newLoc = "A4";
+        type.setLoc(newLoc);
+
+        Assertions.assertEquals(newLoc, type.getLoc());
     }
 
 }
